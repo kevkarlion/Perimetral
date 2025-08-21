8fd12e4328d7fc4d529232749a3c4fa378e4bc27
Kevin Riquelme <kriquelme10@gmail.com>
2025-08-21 16:45:04 -0300

    evitar el prerenderizado

--- app/(main)/catalogo/variants/page.tsx
+++ app/(main)/catalogo/variants/page.tsx
@@ -2,13 +2,16 @@
 import { useProductStore } from '@/app/components/store/product-store'
 import VariantPage from '@/app/components/VariantPage/VariantPage'
 
+// Esto evita que Next.js intente prerenderizar estáticamente
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
   const URL = process.env.BASE_URL
   const res = await fetch(`${URL}/api/stock/${id}`, { cache: 'no-store' })
   const { data: product } = await res.json()
 
-  // Hidratar el store en cliente solo si no está cargado
   if (typeof window !== 'undefined') {
     useProductStore.setState(state => ({
       products: state.products.length ? state.products : [product],
@@ -16,8 +19,5 @@ export default async function Page({ params }: { params: Promise<{ id: string }>
     }))
   }
 
-  return <VariantPage  />
+  return <VariantPage />
 }
-
-
-
